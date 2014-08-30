482ade1917a52a24cf71b0e93f8832848bd7648b
aenrosello <aenrosello@gmail.com>
2014-08-30 12:30:08 -0600

    initial commit

---
+++ PromiseTest/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by anavarro on 30/08/14.
+ */
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('<html><body><h1>Hello World</h1></body></html>');
+}).listen(3000);
+
+console.log('Server running on port 3000.');
